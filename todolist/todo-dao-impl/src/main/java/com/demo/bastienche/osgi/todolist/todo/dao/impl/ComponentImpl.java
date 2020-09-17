88a97799571fb7c7830905048c4aaa9e5defccc7
Bastien CHEVRIAUT <bastien.chevriaut@sii.fr>
2020-09-17 17:11:25 +0200

    adding bundle DAO implementation

---
+++ todolist/todo-dao-impl/src/main/java/com/demo/bastienche/osgi/todolist/todo/dao/impl/ComponentImpl.java
@@ -0,0 +1,10 @@
+package com.demo.bastienche.osgi.todolist.todo.dao.impl;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class ComponentImpl {
+    
+    //TODO add an implementation
+    
+}
---
+++ todolist/todo-dao-impl/src/test/java/com/demo/bastienche/osgi/todolist/todo/dao/impl/UnitTest.java
@@ -0,0 +1,12 @@
+package com.demo.bastienche.osgi.todolist.todo.dao.impl;
+
+import org.junit.Test;
+
+public class UnitTest {
+    
+    @Test
+    public void testSomething() {
+        //TODO add an implementation
+    }
+    
+}
