1223cb1ae5194bbdb50097c748c647c053de46bc
Bastien CHEVRIAUT <bastien.chevriaut@sii.fr>
2020-09-17 16:49:45 +0200

    creating dao bundle

---
+++ todolist/dao-api/src/main/java/com/demo/bastienche/osgi/todolist/dao/ConsumerInterface.java
@@ -0,0 +1,15 @@
+package com.demo.bastienche.osgi.todolist.dao;
+
+import org.osgi.annotation.versioning.ConsumerType;
+
+/**
+ * This is an interface which is designed to be implemented by users
+ * of this API rather than by implementations of the API. A good
+ * example of a consumer interface is <code>javax.servlet.Servlet</code>
+ */
+@ConsumerType
+public interface ConsumerInterface {
+    
+    //TODO add an API
+    
+}
---
+++ todolist/dao-api/src/main/java/com/demo/bastienche/osgi/todolist/dao/ProviderInterface.java
@@ -0,0 +1,16 @@
+package com.demo.bastienche.osgi.todolist.dao;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * This is an interface which is designed to be implemented by providers
+ * of this API rather than by users of the API. A good example of a
+ * provider interface is <code>javax.servlet.ServletContext</code>
+ */
+
+@ProviderType
+public interface ProviderInterface {
+    
+    //TODO add an API
+    
+}
