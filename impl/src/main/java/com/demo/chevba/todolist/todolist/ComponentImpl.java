e0b5e198b43816799ce946a9ede83613ed7ae5a7
Bastien CHEVRIAUT <bastien.chevriaut@sii.fr>
2020-09-17 16:21:56 +0200

    initial commit

---
+++ impl/src/main/java/com/demo/chevba/todolist/todolist/ComponentImpl.java
@@ -0,0 +1,10 @@
+package com.demo.chevba.todolist.todolist;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class ComponentImpl {
+    
+    //TODO add an implementation
+    
+}
---
+++ impl/src/test/java/com/demo/chevba/todolist/todolist/UnitTest.java
@@ -0,0 +1,12 @@
+package com.demo.chevba.todolist.todolist;
+
+import org.junit.Test;
+
+public class UnitTest {
+    
+    @Test
+    public void testSomething() {
+        //TODO add an implementation
+    }
+    
+}
