2fef5f7fb3efa511679a2f9f5122c9a8fe8f9664
Bastien CHEVRIAUT <bastien.chevriaut@sii.fr>
2020-09-18 08:46:47 +0200

    creating the OSGI rest bundle

---
+++ rest-service/src/test/java/com/demo/bastienche/osgi/todolist/rest/UnitTest.java
@@ -0,0 +1,12 @@
+package com.demo.bastienche.osgi.todolist.rest;
+
+import org.junit.Test;
+
+public class UnitTest {
+    
+    @Test
+    public void testSomething() {
+        //TODO add an implementation
+    }
+    
+}
